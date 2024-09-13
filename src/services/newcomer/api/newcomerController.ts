67ffe2d51f4fc5a9ce5cc89a6f9f590ec0c83492
TepanD <stefanwork12@gmail.com>
2024-09-13 14:00:10 +0700

    -refactor: extract generate newcomer ID code from insertNewcomer

--- src/services/newcomer/api/newcomerController.ts
+++ src/services/newcomer/api/newcomerController.ts
@@ -3,6 +3,7 @@ import sheetsRepo from "../../googleSheets/repository/sheetsRepo";
 import numberHelper from "../../../libraries/helpers/numberHelper";
 import { config } from "../../../config/config";
 import logger from "../../../libraries/logger/logger";
+import type { sheets_v4 } from "googleapis";
 
 const SHEET_NAME = config.GOOGLE_SHEET_NAME ?? "";
 
@@ -28,23 +29,29 @@ const insertNewcomer = async (
 		return returnResponse;
 	}
 
-	const ids = (await sheetsRepo.getRowValue(
+	// const ids = (await sheetsRepo.getRowValue(
+	// 	sheetsService,
+	// 	spreadSheetId,
+	// 	"test_byWA!A2:A"
+	// )) ?? [[0]];
+	// let numberizedIds: number[] = [];
+	// if (ids.length == 1 && ids[0][0] == 0) {
+	// 	numberizedIds = [0];
+	// } else {
+	// 	numberizedIds = ids.map((value) => numberHelper.getDigit(value[0]) ?? 0);
+	// }
+	// const lastIdNumber = Math.max(...numberizedIds);
+
+	// //do not change parenthesis, calculation might differ
+	// const newcomerId =
+	// 	"UNI" + ((lastIdNumber ?? 0) + 1).toString().padStart(5, "0");
+	const generateNewcomerIdResponse = await generateNewComerID(
 		sheetsService,
 		spreadSheetId,
-		"test_byWA!A2:A"
-	)) ?? [[0]];
-	let numberizedIds: number[] = [];
-	if (ids.length == 1 && ids[0][0] == 0) {
-		numberizedIds = [0];
-	} else {
-		numberizedIds = ids.map((value) => numberHelper.getDigit(value[0]) ?? 0);
-	}
-	const lastIdNumber = Math.max(...numberizedIds);
-
-	//do not change parenthesis, calculation might differ
-	const newcomerId =
-		"UNI" + ((lastIdNumber ?? 0) + 1).toString().padStart(5, "0");
-	const data = [newcomerId, ...newcomerData];
+		returnResponse
+	);
+	if (!generateNewcomerIdResponse.isSuccess) return generateNewcomerIdResponse;
+	const data = [generateNewcomerIdResponse.message ?? "", ...newcomerData];
 
 	try {
 		const insertResult = await sheetsRepo.insertNewRow(
@@ -70,6 +77,53 @@ const insertNewcomer = async (
 		returnResponse.message = err.toString();
 		return returnResponse;
 	}
+};
+
+const generateNewComerID = async (
+	sheetsService: sheets_v4.Sheets,
+	spreadSheetId: string,
+	returnResponse: ResponseHelper
+): Promise<ResponseHelper> => {
+	const newRowIndex = await sheetsRepo.getAfterLastRowIndex(
+		sheetsService,
+		spreadSheetId,
+		SHEET_NAME
+	);
+	if (newRowIndex == null) {
+		returnResponse.isSuccess = false;
+		returnResponse.message =
+			"Last newcomer ID not found, please check google sheet.";
+		return returnResponse;
+	}
+
+	const ids = (await sheetsRepo.getRowValue(
+		sheetsService,
+		spreadSheetId,
+		"test_byWA!A2:A"
+	)) ?? [[0]];
+	let numberizedIds: number[] = [];
+	if (ids.length == 1 && ids[0][0] == 0) {
+		numberizedIds = [0];
+	} else {
+		numberizedIds = ids.map((value) => numberHelper.getDigit(value[0]) ?? 0);
+	}
+	const lastIdNumber = Math.max(...numberizedIds);
+
+	const today = new Date();
+	const year = today.getFullYear().toString();
+	const month = today.getMonth().toString().padStart(2, "0");
+	const date = today.getDate().toString().padStart(2, "0");
+
+	//do not change parenthesis, calculation might differ
+	const newcomerId =
+		"UNI" +
+		year +
+		month +
+		date +
+		((lastIdNumber ?? 0) + 1).toString().padStart(3, "0");
+	returnResponse.isSuccess = true;
+	returnResponse.message = newcomerId;
+	return returnResponse;
 };
 
 const newcomerController = {
