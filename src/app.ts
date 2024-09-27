6318426f89e1024b1f027e2ad580279bf7251227
TepanD <stefanwork12@gmail.com>
2024-09-27 12:05:07 +0700

    -chore: update app

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,12 @@ import { onMessage } from "./services/whatsAppWeb/api/onMessageEvents";
 import { onShutDown } from "./appEvents/onShutDown";
 
 export const app = () => {
-	writeCredentials();
-	connectClient();
-	onMessage();
-	onShutDown();
+	try {
+		writeCredentials();
+		connectClient();
+		onMessage();
+		onShutDown();
+	} catch (e: any) {
+		console.log(e.message);
+	}
 };
