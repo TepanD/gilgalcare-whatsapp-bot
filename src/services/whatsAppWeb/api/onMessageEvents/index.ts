b561456292fad4c9efbcf41e4d6a3c2e13dffef1
TepanD <stefanwork12@gmail.com>
2024-10-15 16:23:42 +0700

    refactor-session-validation

--- src/services/whatsAppWeb/api/onMessageEvents/index.ts
+++ src/services/whatsAppWeb/api/onMessageEvents/index.ts
@@ -1,10 +1,8 @@
+import { validateSession } from "./../../../../services/session";
 import { config } from "../../../../config/config";
 import { client } from "../../client";
 import chatEvents from "./chatEvents";
 import newcomerEvents from "./newcomerEvents";
-import { sessions } from "./../../../../db/schema";
-import { db } from "./../../../../db/index";
-import { eq, sql, count } from "drizzle-orm";
 
 const SHEET_ID = config.GOOGLE_SHEETS_ID ?? "";
 const GROUP_NAME = config.WHATSAPP_GROUP_NAME ?? "";
@@ -27,7 +25,7 @@ export const onMessage = async () => {
 			}
 		});
 
-		const newSession: Boolean = await validateSessionNew(senderNumber, chatId);
+		const newSession: Boolean = await validateSession(senderNumber, chatId);
 		const spreadSheetId = SHEET_ID;
 
 		//validate chat is group && group name
@@ -106,43 +104,4 @@ export const onMessage = async () => {
 			}
 		}
 	});
-};
-
-const validateSessionNew = async (
-	senderNumber: string,
-	chatId: string
-): Promise<Boolean> => {
-	const sessionsFound = await db
-		.select({ count: count() })
-		.from(sessions)
-		.where(eq(sessions.phoneNumber, senderNumber));
-
-	await db
-		.delete(sessions)
-		.where(
-			sql`${sessions.expiredDatetime} <= strftime('%s', 'now', '+7 hours')`
-		)
-		.returning();
-
-	if (sessionsFound[0].count === 1) {
-		// pakai expired date untuk kirim lagi chat "hi", mungkin expirednya kasih agak lama?
-		await db
-			.update(sessions)
-			.set({
-				expiredDatetime: sql`(strftime('%s', 'now', '+7 hours') + 600)`,
-			})
-			.where(eq(sessions.phoneNumber, senderNumber));
-
-		return false;
-	} else if (sessionsFound[0].count === 0) {
-		await db.insert(sessions).values({
-			chatId: chatId,
-			phoneNumber: senderNumber,
-			expiredDatetime: sql`(strftime('%s', 'now', '+7 hours') + 600)`,
-		});
-
-		return true;
-	}
-
-	return false;
 };
