4d0ddeb69c1013f62e37b090880907253058709f
TepanD <stefanwork12@gmail.com>
2024-10-15 21:34:31 +0700

    refactor: prepare code for production

--- src/services/whatsAppWeb/api/onMessageEvents/index.ts
+++ src/services/whatsAppWeb/api/onMessageEvents/index.ts
@@ -5,17 +5,18 @@ import { adminEvents } from "./adminEvents";
 import { userEvents } from "./userEvents";
 
 const SHEET_ID = config.GOOGLE_SHEETS_ID ?? "";
-const GROUP_NAME = config.WHATSAPP_GROUP_NAME ?? "";
+// const GROUP_NAME = config.WHATSAPP_GROUP_NAME ?? "";
 let IS_ADMIN = false;
 
 export const onMessage = async () => {
 	client.on("message_create", async (msg) => {
-		//get chat data.
 		const adminNumbers = config.ADMIN_WA_NUMBER;
+		//get chat data.
 		const chatData = await msg.getChat();
 		const chatId = chatData.id._serialized;
 		const senderNumber = (await msg.getContact()).number;
 
+		//validate role
 		adminNumbers.forEach((element) => {
 			if (element === senderNumber) {
 				IS_ADMIN = true;
@@ -28,11 +29,13 @@ export const onMessage = async () => {
 		const newSession: boolean = await validateSession(senderNumber, chatId);
 		const spreadSheetId = SHEET_ID;
 
-		//validate chat is group && group name
-		if (chatData.isGroup && chatData.name === GROUP_NAME && IS_ADMIN) {
+		//to validate chat is group && group name use:
+		//chatData.isGroup && chatData.name === GROUP_NAME &&
+
+		if (IS_ADMIN) {
 			await adminEvents(msg, spreadSheetId);
 			return;
-		} else if (chatData.isGroup && chatData.name === GROUP_NAME && !IS_ADMIN) {
+		} else if (!IS_ADMIN) {
 			await userEvents(msg, client, chatId, spreadSheetId, newSession);
 			return;
 		}
